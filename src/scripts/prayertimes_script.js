b0892cb4c14081656de6f1692870ae3d791909f7
Muhammad Ali <m.ali023@hotmail.com>
2022-06-23 15:33:27 -0400

    Rewrite .toLocaleString method calls & store its locales argument as const

--- src/scripts/prayertimes_script.js
+++ src/scripts/prayertimes_script.js
@@ -6,20 +6,28 @@ const ID_YEAR_H = document.getElementById("year-h");
 const ID_FULL_DATE_G = document.getElementById("full-date-g");
 const ID_DATE_H = document.getElementById("date-h");
 
+const hijri_locale = "ar-SA-u-islamic-umalqura";
+const masjid_locale = "en-CA";
+
+
 var date = new Date();
+
 
 // setTimeout(updateSolar,(milliseconds between midnight and current time))
 // setTimeout(updateHijri, (ms between maghrib and currentime))
 
+
 //All solar current day value initializations
+
 ID_FULL_DATE_G.textContent = date.toLocaleDateString();
 
 
 //All hijree current day value initializations
-ID_YEAR_H.textContent = date.toLocaleString("ar-SA-u-ca-islamic-umalqura", {year:"numeric"})
-ID_MONTH_H.textContent = date.toLocaleString("ar-SA-u-ca-islamic-umalqura", {month:"long"})
-ID_WEEKDAY.textContent = date.toLocaleString("ar-SA-u-ca-islamic-umalqura", {weekday:"long"})
-ID_DATE_H.textContent = date.toLocaleString("ar-SA-u-ca-islamic-umalqura", {day:"2-digit"})
+
+ID_YEAR_H.textContent = date.toLocaleString(hijri_locale, {year:"numeric"})
+ID_MONTH_H.textContent = date.toLocaleString(hijri_locale, {month:"long"})
+ID_WEEKDAY.textContent = date.toLocaleString(hijri_locale, {weekday:"long"})
+ID_DATE_H.textContent = date.toLocaleString(hijri_locale, {day:"2-digit"})
 
 
 setInterval(updateClock, 1000);
@@ -28,7 +36,7 @@ function updateClock() {
 
 	date = new Date();
 
-	ID_TIME.textContent = date.toLocaleString("en-CA", {hour: "2-digit", minute: "2-digit", second: "2-digit"}).substring(0, 8);
+	ID_TIME.textContent = date.toLocaleString(masjid_locale, {hour: "2-digit", minute: "2-digit", second: "2-digit"}).substring(0, 8);
 
 }
 
@@ -37,6 +45,7 @@ function updateSolar() {
 	setInterval(updateSolar, 3600000);
 
 	//Add all updates that happen at midnight
+
 	ID_FULL_DATE_G.textContent = date.toLocalDateString();
 
 }
@@ -46,6 +55,7 @@ function updateHijri() {
 	//setTimeout(ms left until tommorow's maghrib);
 
 	//Add all hijree updates that happen at sunset
-	ID_WEEKDAY.textContent = date.toLocaleString("ar-SA-u-ca-islamic-umalqura", {weekday:"long"});
-	
+
+	ID_WEEKDAY.textContent = date.toLocaleString(masjid_locale, {weekday:"long"});
+
 }
